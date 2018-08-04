ec4032640a34d6dba10513d09ed9d459d773789f
rdey <rdey@vmware.com>
2018-08-03 23:04:27 -0700

    image-def

--- app/build_docker_image.sh
+++ app/build_docker_image.sh
@@ -30,4 +30,7 @@ mkdir -p build
 generate_config_json
 
 docker build --build-arg APP_PORT=$port -t $USERNAME/$IMAGE:latest .
+
+eval "echo '[{\"name\":\"$IMAGE:latest\",\"imageUri\":\"$USERNAME/$IMAGE:latest\"}]'" > imagedefinitions.json
+#echo $image_def  > imagedefinitions.json
 echo "All Good"
